4e2ae2ef4dd4deb098e77052f17ad97f835d7277
Flávio Luiz Mendes <flmendes@gmail.com>
2020-09-25 13:52:53 -0300

    Ajuste na mensagem do PHP

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/../vendor/autoload.php';
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello Jenkins X World!!!, I'm a Slim App!");
+    $response->getBody()->write("Hello AWS!!!, I'm a Slim App!");
     return $response;
 });
 
